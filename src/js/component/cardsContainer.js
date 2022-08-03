a6bc83d5cc3755bf5b4a6d6a50fdea26489a877c
Mirian Barajas <mirianbarcam@gmail.com>
2022-08-03 19:25:18 +0000

    maquetación sin margenes

---
+++ src/js/component/cardsContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import  Card  from "./card.js";
+import "../../styles/cardsContainer.css";
+
+ const CardsContainer = () => (
+  <div className="">
+    <h1>TITULO</h1>
+    <div className="container-card">
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+    </div>
+  </div>
+);
+export default CardsContainer;
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import IconoStarWars from "../../img/iconoStarWars.png";
+import IconoRickAndMorty from "../../img/Rick_and_Morty.png";
 import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar mb-3">
 			<Link to="/">
-				<img className="navbar-brand mb-0 h1 icon_starWars" src={IconoStarWars}></img>
+				<img className="navbar-brand mb-0 h1 icon_starWars" src={IconoRickAndMorty}></img>
 			</Link>
-			<div className="ml-auto">
+			<div className="container_btn_favourite">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button type="button" className="btn_favourite" >Favourites
+					<span className="badge text-bg-secondary">4</span>
+					</button>
 				</Link>
 			</div>
 		</nav>
